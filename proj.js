284169b81a13058e03bf7454a9e076d215ea2ab8
Shelby <miguel.manarin@escola.pr.gov.br>
2026-06-03 08:21:44 -0300

    Update proj.js

--- proj.js
+++ proj.js
@@ -38,16 +38,16 @@ function calculaTempo(tempoObjetivo) {
     let segundos = Math.floor(tempoFinal / 1000);
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
-    let dias = Math.floor(horas / 60);
+    let dias = Math.floor(horas / 24);
 
     segundos %= 60;
     minutos %= 60;
-    horas %= 60;
+    horas %= 24;
 
     if (tempoFinal > 10) {
         return [dias, horas, minutos, segundos];
     } else {
-        return [05, 72, 54, 10];
+        return [0, 0, 0, 0];
     }
 }
 
